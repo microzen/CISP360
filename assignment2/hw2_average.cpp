dfb6f342938493f787c1b5d22c342bf043d9a388
Microzenas <microzenas@gmail.com>
2022-08-30 18:31:57 -0700

    assignment 2 average

---
+++ assignment2/hw2_average.cpp
@@ -0,0 +1,21 @@
+
+#include "iostream"
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int num1 = 28;
+    int num2 = 32;
+    int num3 = 37;
+    int num4 = 24;
+    int num5 = 33;
+
+    cout << "Number 1: " << num1 << endl;
+    cout << "Number 1: " << num2 << endl;
+    cout << "Number 1: " << num3 << endl;
+    cout << "Number 1: " << num4 << endl;
+    cout << "Number 1: " << num5 << endl;
+
+    double sum = (num1 + num2 + num3 + num4 + num5) / 5.0;
+    cout << "Average: " << sum << endl;
+    return 0;
+}
