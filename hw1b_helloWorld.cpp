973f009a6a0ba3b3ca5c53adc3a792194f049689
Microzenas <microzenas@gmail.com>
2022-08-30 18:14:47 -0700

    Init Local and ignore files

---
+++ hw1b_helloWorld.cpp
@@ -0,0 +1,21 @@
+/*****************************************************
+Student name: Yezhi Wu
+Student ID: w1761432
+Course #: CISP360 
+Section #: 25324 & 24499
+Instruction mode: Online or Hybrid
+*****************************************************
+Assignment #1b - Hello World
+Source file name: hw1b_helloWorld.cpp
+Program description: This will display a “Hello World” message
+*****************************************************/
+
+#include <iostream>
+using namespace std;
+
+//main function
+int main()
+{
+	cout << "Hello World!" << endl << endl;
+	return 0;
+}
