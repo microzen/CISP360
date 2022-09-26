af75bbb8563a2ea6c61224d340f5e55468e48598
Microzenas <38436162+microzen@users.noreply.github.com>
2022-09-26 04:32:40 +0000

    Assignment 5

--- assignment5/mathTutor/hw5_MathTutor.cpp
+++ assignment5/mathTutor/hw5_MathTutor.cpp
@@ -1,11 +1,21 @@
+/*****************************************************
+Student name: Regina Gil, Yezhi Wu
+Student ID: w1468676 , w1761432
+Course #: CISP360
+Section #: 16659
+Instruction mode: Online
+*****************************************************
+Assignment #5 -  Math Tutor
+Source file name: hw5_MathTutor.cpp
+Program description: Display a menu to allow the user to select an addition, subtraction, multiplication, or division problem. The final selection on the menu should let the user quit the program. After the user has finished the math problem, the program should display the menu again. This process is repeated until the user chooses to quit the program.
+*****************************************************/
 #include <cmath>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 using namespace std;
 
 /// @brief Options of user's choice.
-enum option
-{
+enum option {
   MT_ADDITION = 1,
   MT_SUBTRACTION,
   MT_MULTIPLICATION,
@@ -20,17 +30,15 @@ option choiceModule();
 bool checkModule(option mt_operator);
 // double calculate(double num1, double num2, option mt_operator);
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
   option op;
   double num1, num2;
   double input;
-  do
-  {
+  do {
     op = choiceModule();
     if (op == MT_QUIT) // if input is 5, quit
     {
-      cout << "Thank you for using Math Tutor." << endl; 
+      cout << "Thank you for using Math Tutor." << endl;
       break;
     }
     checkModule(op);
@@ -39,10 +47,9 @@ int main(int argc, char const *argv[])
   return 0;
 }
 
-/// @brief To ask users for input to choose a option for calculate.
+/// @brief To ask users for input to choose an option for calculation.
 /// @return 1.Addition , 2.Subtraction, 3.Multiplication, 4.Division, 5.Quit,
-option choiceModule()
-{
+option choiceModule() {
   // #TODO: choice module
   int input;
   cout << "\n\tMath Tutor Menu" << endl;
@@ -54,21 +61,17 @@ option choiceModule()
   cout << "5. Quit Program " << endl;
   cout << "----------------------------" << endl;
 
-  do
-  {
+  do {
     input = 0;
     cout << "Input your choice, then press [Enter]: ";
     cin >> input;
-    if (input >= 1 && input <= 5)
-    {
-      // if input is 1-5, stop to loop
+    cin.clear(); // clear input for the illegal number.
+    cin.ignore(100, '\n');
+    if (input >= 1 && input <= 5) {
+      // if input is 1-5, stop the loop
       break;
     }
     cout << "\nThe valid choices are 1-5. Please try again." << endl;
-
-    cin.clear(); // clear input for the illegal number.
-    cin.ignore(100, '\n');
-
   } while (true);
 
   return static_cast<option>(input);
@@ -77,15 +80,12 @@ option choiceModule()
 /// @brief Get a random divisible number
 /// @param number
 /// @return
-int randDivisible(int number)
-{
-  int divisibles[number / 2];
+int randDivisible(int number) {
+  int divisibles[number / 2 + 1];
   int langht = 0;
   divisibles[langht++] = number;
-  for (size_t i = MIN_RAND; i < number / 2; i++)
-  {
-    if (number % i == 0)
-    {
+  for (size_t i = MIN_RAND; i <= (number / 2); i++) {
+    if (number % i == 0) {
       divisibles[langht] = i;
       langht++;
     }
@@ -98,14 +98,12 @@ int randDivisible(int number)
 /// @param num2 number 2
 /// @param mt_operator operator
 /// @return Calculation result
-double calculate(double num1, double num2, option mt_operator)
-{
+double calculate(double num1, double num2, option mt_operator) {
   // if (mt_operator == MT_QUIT)
   //   throw "Operator only between 1-4";
   // you code
   double result = 0;
-  switch (mt_operator)
-  {
+  switch (mt_operator) {
   case MT_ADDITION:
     result = num1 + num2;
     break;
@@ -116,8 +114,7 @@ double calculate(double num1, double num2, option mt_operator)
     result = num1 * num2;
     break;
   case MT_DIVISION:
-    if (num2 == 0)
-    {
+    if (num2 == 0) {
       throw "The denominator can not be zero.";
     }
     result = num1 / num2;
@@ -130,9 +127,8 @@ double calculate(double num1, double num2, option mt_operator)
 }
 
 /// @brief To ask users to input a number then check the result;
-/// @return isCorret
-bool checkModule(option mt_operator)
-{
+/// @return isCorrect
+bool checkModule(option mt_operator) {
   const char operators[4] = {'+', '-', '*', '/'};
 
   // random numbers
@@ -140,35 +136,28 @@ bool checkModule(option mt_operator)
   int user_answer; // answer
   srand(time(NULL));
   num1 = rand() % (MAX_RAND - MIN_RAND) + MIN_RAND;
-  if (mt_operator == MT_DIVISION)
-  {
+  if (mt_operator == MT_DIVISION) {
     num2 = randDivisible(num1);
-  }
-  else
-  {
+  } else {
     num2 = (rand() % (MAX_RAND - MIN_RAND)) + MIN_RAND;
   }
 
-  if(num2 > num1){
+  if (num2 > num1) {
     throw "number 2 > 1";
   }
 
   double result = calculate(num1, num2, mt_operator); // calculation
 
   // input answer and display info
-  cout << setw(5) << num1 << "\n"
-       << operators[mt_operator - 1];
+  cout << setw(5) << num1 << "\n" << operators[mt_operator - 1];
   cout << setw(4) << num2 << endl;
   cout << "----------------------------" << endl;
   cin >> user_answer;
 
-  if (result == user_answer)
-  {
+  if (result == user_answer) {
     cout << "\nCongratulations! That's right." << endl;
     return true;
-  }
-  else
-  {
+  } else {
     cout << "\nSorry, the correct answer is : " << result << endl;
     return false;
   }
--- assignment5/sum/hw5_Sum.cpp
+++ assignment5/sum/hw5_Sum.cpp
@@ -1,3 +1,14 @@
+/*****************************************************
+Student name: Regina Gil, Yezhi Wu
+Student ID: w1468676 , w1761432
+Course #: CISP360
+Section #: 16659
+Instruction mode: Online
+*****************************************************
+Assignment #5 -  Sum of Numbers 
+Source file name: hw5_Sum.cpp
+Program description: A program that asks the user for a positive integer value. The program should use a loop to get the sum of all the integers from 1 up to the number entered.
+*****************************************************/
 #include <iostream>
 #include <time.h>
 
@@ -7,9 +18,8 @@ long long sequence(int number);
 int inputNumber();
 void outputModule(long long result, int number);
 
-int main(int argc, char const *argv[])
-{
-  clock_t cl;       // clock
+int main(int argc, char const *argv[]) {
+  clock_t cl;   // clock
   cl = clock(); // start time
 
   int number;       // user's input
@@ -25,38 +35,32 @@ int main(int argc, char const *argv[])
   // convert clock_t to seconds on the double type.
   seconds = (static_cast<double>(cl)) / CLOCKS_PER_SEC;
   cout << "----------------------------------" << endl;
-  cout << "Process exited after "<<seconds<< " seconds with return value 0";
+  cout << "Process exited after " << seconds << " seconds with return value 0";
 
   return 0;
 }
 /// @brief a sequence to calculate 1 to a number.
 /// @param number a number that must greater than 0.
 /// @return result
-long long sequence(int number)
-{
+long long sequence(int number) {
   if (number < 0)
     throw "It can't be a negative number.";
   long long result = 0;
   // result = (h * (h + 1)) / 2;
-  for (size_t i = 1; i <= number; i++)
-  {
+  for (size_t i = 1; i <= number; i++) {
     result += i;
   }
   return result;
 }
 /// @brief ask user to input a number and the number must greater than 0.
 /// @return input result
-int inputNumber()
-{
-  // #TODO: Could you help me to finish the ouput moudle?
+int inputNumber() {
   int num;
-  do
-  {
+  do {
     cout << "\nPlease input a number greater than 0, then press [Enter]"
          << endl;
     cin >> num;
-    if (num < 0)
-    {
+    if (num < 0) {
       cout << "The number must greater than 0" << endl;
       continue;
     }
@@ -64,11 +68,10 @@ int inputNumber()
   } while (true);
 }
 
-/// @brief Output the result that calcuated
+/// @brief Output the result that was calculated
 /// @param result The result of the sum
 /// @param number The number user inputed
-void outputModule(long long result, int number)
-{
-  // #TODO: Could you help me to finish the ouput moudle?
-  cout << "The sum of numbers 1 - " << number << " is " << result << "." << endl;
+void outputModule(long long result, int number) {
+  cout << "The sum of numbers 1 - " << number << " is " << result << "."
+       << endl;
 }
